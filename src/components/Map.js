2e47fcb9bb986c6b93dfb172bd4faa79a8d70359
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-04-10 15:53:51 +0100

    Added marker on the charts

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import data from '../assets/mapData';
 
 const Map = () => {
-    const position = [ 51.5171566 ,-0.1202222]
+    const position = [51.5130972 ,-0.1292559]
+    console.log(data)
     // set central london as a the center position
     return(
       <div>
-      <MapContainer center={position} zoom={10} scrollWheelZoom={true}>
+      <MapContainer center={position} zoom={14} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        {/* <Marker position={position}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker> */}
+        {
+        data.map((dat) => {
+            console.log(dat.lat ,dat.lng)
+        return (
+            <Marker position={[dat.lat ,dat.lng]}>
+            <Popup>
+               lat: {dat.lat} <br /> lng: {dat.lng}
+            </Popup>
+            </Marker> 
+        )
+        })
+        }
       </MapContainer>
       </div>
     )
