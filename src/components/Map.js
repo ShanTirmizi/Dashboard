9f733f9f7d64d1a8fec17c03089c2bfe9ba16d6c
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2021-04-11 19:22:51 +0100

    Fixed the CSS

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,32 +5,25 @@ import data from '../assets/mapData';
 const Map = () => {
     const position = [51.5130972 ,-0.1292559]
     console.log(data)
-    // set central london as a the center position
     return(
       <div>
-      <MapContainer center={position} zoom={14} scrollWheelZoom={true}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker> */}
-        {
-        data.map((dat) => {
-            console.log(dat.lat ,dat.lng)
-        return (
-            <Marker position={[dat.lat ,dat.lng]}>
-            <Popup>
-               lat: {dat.lat} <br /> lng: {dat.lng}
-            </Popup>
-            </Marker> 
-        )
-        })
-        }
-      </MapContainer>
+        <MapContainer center={position} zoom={15} scrollWheelZoom={true}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            data.map((dat) => {
+              return (
+                <Marker position={[dat.lat ,dat.lng]}>
+                  <Popup>
+                    lat: {dat.lat} <br /> lng: {dat.lng}
+                  </Popup>
+                </Marker> 
+              )
+            })
+          }
+        </MapContainer>
       </div>
     )
 }
